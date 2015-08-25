470a5925fb1e6c342c228cba1ebb52e67a86c75b
Joergen Ibsen <jibz-git@stdip.com>
2015-08-25 16:36:16 +0200

    Include own header first to check for include errors

--- spooky.c
+++ spooky.c
@@ -23,10 +23,10 @@
 //   July 30 2012: I reintroduced the buffer overflow
 //   August 5 2012: SpookyV2: d = should be d += in short hash, and remove extra mix from long hash
 
+#include "spooky.h"
+
 #include <memory.h>
 #include <stdbool.h>
-
-#include "spooky.h"
 
 #define ALLOW_UNALIGNED_READS 1
 
